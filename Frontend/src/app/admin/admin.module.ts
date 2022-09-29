1e8d6e9214fdd2af467529f8d845d6f3f724ce1f
4ouR04 <amosmwongelah@gmail.com>
2022-09-30 01:45:12 +0300

    Update routing

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RecordsComponent } from './records/records.component';
+import { HomeComponent } from './home/home.component';
 
-
+const adminRoutes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'records',
+        component: RecordsComponent,
+      }
+    ]
+  },
+];
 
 @NgModule({
-  declarations: [
-    AdminComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AdminComponent, RecordsComponent, HomeComponent],
+  imports: [CommonModule, RouterModule.forChild(adminRoutes)],
 })
-export class AdminModule { }
+export class AdminModule {}
