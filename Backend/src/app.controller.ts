b11dc1f6c30c6b7fb09d43bdced4c7aa8a85c3c2
4ouR04 <amosmwongelah@gmail.com>
2022-10-08 20:19:47 +0300

    Get one client with id

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { identity } from 'rxjs';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getClients(): string {
     return this.appService.getClients();
+  }
+
+  @Get(':id')
+  getClient(@Param('id') clientId:string) {
+    
   }
 }
