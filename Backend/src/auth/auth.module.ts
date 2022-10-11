f66b82343fab6841e5e17e14c56ccfdd4820557e
4ouR04 <amosmwongelah@gmail.com>
2022-10-11 14:17:05 +0300

    Update services

--- Backend/src/auth/auth.module.ts
+++ Backend/src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schemas/user.schema';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'user',schema: UserSchema}])],
   controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- Backend/src/bills/bills.module.ts
+++ Backend/src/bills/bills.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BillSchema } from 'src/schemas/bill.schema';
 import { BillsController } from './bills.controller';
+import { BillsService } from './bills.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: 'bill', schema: BillSchema}])],
-  controllers: [BillsController]
+  controllers: [BillsController],
+  providers: [BillsService]
 })
 export class BillsModule {}
