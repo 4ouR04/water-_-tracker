7bc0dd29cad1640d82b0e2234b6bf5bee3e1f8c4
4ouR04 <amosmwongelah@gmail.com>
2022-11-26 20:32:22 +0300

    Fix auth routes

--- Frontend/src/app/auth/login/login.component.ts
+++ Frontend/src/app/auth/login/login.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  isLoginMode = true
-  constructor() { }
+  isLoginMode = true;
+  mode: string = '';
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    this.router.navigate(['/auth/login']);
   }
-  onSubmit(f: NgForm){
-    console.log(f.value)
+  onSubmit(f: NgForm) {
+    console.log(f.value);
   }
-  onSwitch(){
-    this.isLoginMode = !this.isLoginMode
+  onSwitch() {
+    this.route.params.subscribe((params) => {
+      this.mode = params['mode'];
+    });
+    if (this.mode == 'login') {
+      this.router.navigate(['/auth/signup'], {});
+      this.isLoginMode = !this.isLoginMode;
+    } else {
+      this.router.navigate(['/auth/login'], {});
+      this.isLoginMode = !this.isLoginMode;
+    }
   }
-
 }
