faf770721d520fcaf1474d2b4eb93546975cb7af
4ouR04 <amosmwongelah@gmail.com>
2022-12-02 17:24:45 +0300

    Sinin users with email and password

--- Frontend/src/app/auth/login/login.component.ts
+++ Frontend/src/app/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { authResponse } from '../models/auth-response.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +13,38 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   isLoginMode = true;
   mode: string = '';
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  message!: string;
+  authObservable!: Observable<authResponse>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private auth: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.router.navigate(['/auth/login']);
   }
   onSubmit(f: NgForm) {
-    console.log(f.value);
+    this.route.params.subscribe((params) => {
+      this.mode = params['mode'];
+    });
+
+    if (this.mode == 'signup') {
+      this.authObservable = this.auth.signup(f.value.email, f.value.password);
+    } else if (this.mode == 'login') {
+      console.log(f.value);
+      this.authObservable = this.auth.login(f.value.email, f.value.password);
+    }
+
+    this.authObservable.subscribe(
+      (resp) => {
+        console.log(resp)
+      },
+      (errorMessage) => {
+        this.message = errorMessage;
+      }
+    );
   }
   onSwitch() {
     this.route.params.subscribe((params) => {
